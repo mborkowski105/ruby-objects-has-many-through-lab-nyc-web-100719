84b26beadcfa88a8fd8502cc1113985777534933
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-14 13:37:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,11 @@
 class Song 
-  @@all = [] 
+  @@all = []
   
-  def initialize
-    
+  attr_reader :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
   end
 end
