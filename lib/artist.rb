60803ab3bc90b57139b566e3828f01c587b2a3db
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-14 13:40:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,18 @@
 class Artist 
   @@all = []
   
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+  
+  def self.all 
+    return @@all
+  end
+  
+  def new_song(name, genre)
+    return Song.new()
+  end
 end
